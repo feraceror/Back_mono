7dab6bc83ef61b726f71cfc9afb2ee8c8b1baa47
feraceror <98607137+feraceror@users.noreply.github.com>
2023-10-01 22:01:54 -0500

    Actualizacion mono secret

--- src/config/config.js
+++ src/config/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const config = {
     port: process.env.PORT || 3000,
-    monoSignature: "whsec_1LZXJSDVuM47hBHr78gCrrfubRYmezum0",
+    monoSignature: "whsec_1XsBotQmDRJVobVJTtI75uL9TTSwtUmSw",
     maxTimeMin: 5
 }; 
 
